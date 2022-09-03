09ddc270b23eabc1647aa0b775fb6d2e70a84016
aedipi <ahmetemredipi@gmail.com>
2022-09-03 22:49:51 +0300

    Initial commit

---
+++ lib/anasayfa.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({Key? key}) : super(key: key);
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+
+  int sayac = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    //test();
+    sayacKontrol();
+  }
+
+  Future<void> sayacKontrol() async {
+    var sp = await SharedPreferences.getInstance();
+
+    sayac = sp.getInt("sayac") ?? 0;
+    setState(() {
+      sayac += 1;//sayac = sayac + 1;
+    });
+    sp.setInt("sayac", sayac);
+  }
+
+  Future<void> test() async {
+    //Tanımlama
+    var sp = await SharedPreferences.getInstance();
+
+    //Kayıt
+    sp.setString("ad", "Ahmet");
+    sp.setInt("yas", 23);
+    sp.setDouble("boy", 1.78);
+    sp.setBool("bekar", true);
+
+    var arkadasListe = <String>[];
+    arkadasListe.add("Ece");
+    arkadasListe.add("Ali");
+
+    sp.setStringList("liste", arkadasListe);
+
+    //Silme
+    //sp.remove("ad");
+
+    //Okuma
+    String gelenAd = sp.getString("ad") ?? "isim yok"; // ?? -> varsayılan değer
+    int gelenYas = sp.getInt("yas") ?? 0;
+    double gelenBoy = sp.getDouble("boy") ?? 0.0;
+    bool gelenBekar = sp.getBool("bekar") ?? false;
+
+    print("Gelen Ad : $gelenAd");
+    print("Gelen Yaş : $gelenYas");
+    print("Gelen Boy : $gelenBoy");
+    print("Gelen Bekar : $gelenBekar");
+
+    var gelenListe = sp.getStringList("liste");
+
+    if (gelenListe != null) {
+      for(var a in gelenListe) {
+        print("Arkadaş : $a");
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Shared Preferences"),
+      ),
+      body: Center(
+        child: Text("Açılış sayısı : $sayac",style: TextStyle(fontSize: 40),),
+      ),
+    );
+  }
+}
